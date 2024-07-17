38f71a8a60ef8024fb69b273156fcf4c3feef85d
Breno L. Lopes <brernolopes@hotmail.com>
2024-07-17 11:07:03 -0300

    adicionada lógica de perseguição

--- ZeldaClone/src/com/eletrocp/entidades/Enemy.java
+++ ZeldaClone/src/com/eletrocp/entidades/Enemy.java
@@ -2,10 +2,27 @@ package com.eletrocp.entidades;
 
 import java.awt.image.BufferedImage;
 
+import com.eletrocp.main.Game;
+
 public class Enemy extends Entity{
+	
+	private double spd = 0.6;
 
 	public Enemy(double x, double y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
+	}
+	public void tick() {
+		if((int)x < Game.player.getX()) {
+			x += spd;
+		} else if ((int)x > Game.player.getX()) {
+			x -= spd;
+		}
+		
+		if((int)y < Game.player.getY()) {
+			y += spd;
+		} else if ((int)y > Game.player.getY()) {
+			y -= spd;
+		}
 	}
 
 }
