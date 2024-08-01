3d8f9c67216588f45eca29fc0596dba99961a445
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-01 15:33:19 -0300

    Adicionando configurações ao menu

--- ZeldaClone/src/com/eletrocp/main/Menu.java
+++ ZeldaClone/src/com/eletrocp/main/Menu.java
@@ -6,21 +6,27 @@ import java.awt.Graphics;
 
 public class Menu {
 	
-	public String[] options = {"Novo Jogo", "Salvar Jogo","Carregar Jogo", "Sair"};
-	
+	public String[] options = {"Novo Jogo", "Salvar Jogo","Carregar Jogo", "Configurações", "Sair"};
+	public String[] configuracoes = {"Video", "Audio", "Voltar"};
+	public String menuState = "NORMAL";
 	public int currentOption = 0;
-	public int maxOption = options.length - 1;
+	public int maxOption = updateMaxOption(options);
 	
 	public boolean up, down, enter;
 	
 	public boolean pause = false;
 	
+	public int updateMaxOption(String[] array) {
+		int currentMaxOption = array.length - 1;
+		return currentMaxOption;
+	}
+	
 	public void tick() {
 		if(up) {
 			up = false;
 			currentOption -= 1;
-			if(currentOption < 0) {
-				currentOption = 0;
+			if(currentOption <= 0) {
+				currentOption = maxOption;
 			}
 		}
 		
@@ -37,7 +43,16 @@ public class Menu {
 			if(options[currentOption] == "Novo Jogo" || options[currentOption] == "Continuar") {
 				Game.gameState = "NORMAL";
 				pause = false;
-			}  else if(options[currentOption] == "Sair") {
+			}  else if(options[currentOption] == "Configurações") {
+				menuState = "CONFIGURACOES";
+				currentOption = 0;
+				maxOption = updateMaxOption(configuracoes);
+			}  else if(configuracoes[currentOption] == "Voltar") {
+				menuState = "NORMAL";
+				currentOption = 0;
+				maxOption = updateMaxOption(options);
+			}
+				else if(options[currentOption] == "Sair") {
 				System.exit(0);
 			}
 		}
@@ -46,34 +61,60 @@ public class Menu {
 	
 	public void render(Graphics g) {
 		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, Game.WIDTH*Game.SCALE, Game.WIDTH*Game.SCALE);
+		g.fillRect(0, 0, Game.WIDTH, Game.WIDTH);
 		g.setColor(Color.white);
 		//Nome
 		g.setFont(new Font("arial", Font.BOLD, 36));
 		
-		g.drawString("RPG CLONE", (Game.WIDTH * Game.SCALE) / 2 - 100, (Game.HEIGHT * Game.SCALE) / 2 - 180);
-		if(pause == false) {
-			g.drawString("Novo Jogo", (Game.WIDTH * Game.SCALE) / 2 - 300, (Game.HEIGHT * Game.SCALE) / 2 - 50);			
-		} else {
-			g.drawString("Continuar", (Game.WIDTH * Game.SCALE) / 2 - 300, (Game.HEIGHT * Game.SCALE) / 2 - 50);
-		}
+		g.drawString("RPG CLONE", (Game.WIDTH) / 2 - 100, (Game.HEIGHT) / 2 - 180);
 		//Novo Jogo
-		//Salvar Jogo
-		g.drawString("Salvar Jogo", (Game.WIDTH * Game.SCALE) / 2 - 300, (Game.HEIGHT * Game.SCALE) / 2 - 10);
-		//Carregar Jogo
-		g.drawString("Carregar Jogo", (Game.WIDTH * Game.SCALE) / 2 - 300, (Game.HEIGHT * Game.SCALE) / 2 + 30);
-		//Sair
-		g.drawString("Sair", (Game.WIDTH * Game.SCALE) / 2 - 300, (Game.HEIGHT * Game.SCALE) / 2 + 70);
 		
-		switch(options[currentOption]) {
-		case "Salvar Jogo": g.drawString(">", (Game.WIDTH * Game.SCALE) / 2 - 330, (Game.HEIGHT * Game.SCALE) / 2 - 10);
+		switch(menuState) {
+		case "CONFIGURACOES":
+			g.drawString("Video", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 - 50);
+			
+			g.drawString("Audio", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 - 6);
+			
+			g.drawString("Voltar", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 + 36);
+			
+			switch(configuracoes[currentOption]) {
+			case "Audio": g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 - 6);
+				break;
+			case "Voltar": g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 + 36);
+				break;
+			default: g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 - 50);	
+    			break;
+			}
+		break;
+		default:
+			if(pause == false) {
+				g.drawString("Novo Jogo", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 - 50);			
+			} else {
+				g.drawString("Continuar", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 - 50);
+			}
+			//Salvar Jogo
+			g.drawString("Salvar Jogo", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 - 6);
+			//Carregar Jogo
+			g.drawString("Carregar Jogo", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 + 36);
+			// Configurações
+			g.drawString("Configurações", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 + 78);
+			//Sair
+			g.drawString("Sair", (Game.WIDTH) / 2 - 300, (Game.HEIGHT) / 2 + 120);
+			
+			switch(options[currentOption]) {
+			case "Salvar Jogo": g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 - 6);
+				break;
+	        case "Carregar Jogo": g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 + 36);
+	        	break;
+	        case "Configurações": g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 + 78);
+	        	break;
+	        case "Sair": g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 + 120);
+	    		break;
+	        default: g.drawString(">", (Game.WIDTH) / 2 - 330, (Game.HEIGHT) / 2 - 50);
+	    		break;
+			}
 			break;
-        case "Carregar Jogo": g.drawString(">", (Game.WIDTH * Game.SCALE) / 2 - 330, (Game.HEIGHT * Game.SCALE) / 2 + 30);
-        	break;
-        case "Sair": g.drawString(">", (Game.WIDTH * Game.SCALE) / 2 - 330, (Game.HEIGHT * Game.SCALE) / 2 + 70);
-    		break;
-        default: g.drawString(">", (Game.WIDTH * Game.SCALE) / 2 - 330, (Game.HEIGHT * Game.SCALE) / 2 - 50);
-    		break;
 		}
+		
 	}
 }
